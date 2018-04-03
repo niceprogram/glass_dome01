d770ea9f9f009f635a4ac7e89ad7a2d7ae437da3
K_Van <niceprogram@yahoo.com>
2018-04-03 21:40:31 +0200

    Update wemos_lolin_BL_serial.ino

--- wemos_lolin_BL_serial.ino
+++ wemos_lolin_BL_serial.ino
@@ -1,14 +1,127 @@
-//HardwareSerial Serial2(2);
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second.
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"'
+
+// Initialize the OLED display using Wire library
+SSD1306  display(0x3c, 5, 4);
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+//float txValue = 0;
+//const int sensorPin = 36; //**************************************************************************CHANGE TO YOUR PIN
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    
+};
 
 void setup() {
   Serial.begin(9600);
-  //Serial2.begin(115200);  // put your setup code here, to run once:
-  //Serial2.begin(9600, SERIAL_8N2, 13, 15); //baud rate, 8 data bits - no parity - 2 stop bits, RX pin, TX pin
+  pinMode(sensorPin, INPUT);
+  display.init();
+  //display.flipScreenVertically();
+  display.setColor(WHITE);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+
+
+  // Create the BLE Device
+  BLEDevice::init("JohnnyFRX ESP32"); // ************************************************Provide Unique Name
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+  display.displayOn();
+  display.clear();
+  display.drawString(64, 20, String("Wait"));
+  display.display();
 }
 
 void loop() {
+  if (deviceConnected) {
+    display.displayOn();
+    
+    char txString[8]; 
+    dtostrf(txValue, 1, 2, txString); 
 
-  Serial.println("<LED, 1, 125, 1>");  // put your main code here, to run repeatedly:
-  //Serial2.println("UART2"); 
-  delay(1000);
+    pCharacteristic->setValue(txString);
+
+    pCharacteristic->notify(); 
+    
+    display.clear();
+    Serial.print(txString);
+    display.drawString(64, 20, String(txString));
+   
+    display.display();
+    
   }
+  else {
+    display.displayOff();
+  };
+  delay(1000);
+}
